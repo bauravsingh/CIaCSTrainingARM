edc10b190aeb7c8f288f1e87a28223b7a5775be9
Baurav Singh <103564761+bauravsingh@users.noreply.github.com>
2023-03-29 11:15:24 +0530

    Update deploy.ps1

--- ContosoFinance-Demo-ARM/deploy.ps1
+++ ContosoFinance-Demo-ARM/deploy.ps1
@@ -5,7 +5,7 @@
 
 
 
-$subscriptionId = "85cd2292-82e3-4c72-a2d7-1ba724a25176" #insert your subscription ID
+$subscriptionId = "726bfeae-0efe-48ac-9acd-d29e8a80da41" #insert your subscription ID
 $resourceGroupName = "BauravCIaCS-rg"  #provide resource group name
 $resourceGroupLocation = "WestEurope"  #location
 
